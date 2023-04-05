939bd68e8cc67fc7aee3c9ba7397b05d36fa805c
Amaroke <70974789+Amaroke@users.noreply.github.com>
2023-04-05 17:03:14 +0200

    Nettoyage du code de kavallieratou.py

--- Kavallieratou/kavallieratou.py
+++ Kavallieratou/kavallieratou.py
@@ -5,31 +5,21 @@ from matplotlib import pyplot as plt
 from skimage import io
 
 # Charger l'image
-img = io.imread(sys.argv[1])
-
-# Convertir l'image en niveau de gris
-gray = img
-
-# Initialisation des seuils
-T_old = 0
-T = np.mean(gray)
+image = io.imread(sys.argv[1])
 
 # On fait 15 itérations
 i = 1
+avg_pixel_val = np.mean(image)
 while i <= 15:
-    T_old = T
 
     # Calcul de la moyenne des valeurs de pixels de l'image
-    avg_pixel_val = np.mean(gray)
+    avg_pixel_val = np.mean(image)
 
     # Soustraction de la moyenne de tous les pixels de l'image
-    gray_sub = gray - avg_pixel_val
+    gray_sub = image - avg_pixel_val
 
     # Égalisation de l'histogramme
     gray_eq = np.uint8(255 * (gray_sub - np.min(gray_sub)) / (np.max(gray_sub) - np.min(gray_sub)))
-
-    # Calcul du nouveau seuil T
-    T = np.mean(gray_eq)
 
     # Enregistrement de l'image binaire
     plt.imsave(f'{sys.argv[1]}_iteration{i}.png', gray_eq, cmap='gray')
@@ -37,11 +27,11 @@ while i <= 15:
     i += 1
 
     # Mise à jour de l'image en niveaux de gris pour la prochaine itération
-    gray = gray_eq
+    image = gray_eq
 
 # Binarisation de l'image
-gray_bin = np.zeros_like(gray)
-gray_bin[gray > T] = 1
+gray_bin = np.zeros_like(avg_pixel_val)
+gray_bin[image > avg_pixel_val] = 1
 
 # Enregistrer l'image finale
 plt.imsave(f'{sys.argv[1]}_iteration.png', gray_bin, cmap='gray')
